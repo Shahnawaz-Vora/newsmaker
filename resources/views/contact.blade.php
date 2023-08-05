9a5d8738ed1e1a472ccd4ea5e9f30540c97d3732
Jaydeep-01 <jaydeepvaghasiya007@example.com>
2023-08-05 10:53:05 +0530

    update in contact us

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,4 +1,22 @@
-<x-layout>
+<x-layout title="Contact Us | Top PR Agency Newsmaker Media and Communications"
+description="Get in touch to get your personalized PR strategies to excel your marketing planes and get better reach. Working with more than 300 media partners to provie you excellent brand experience. "
+keywords="PR services, public relations, media relations, strategic storytelling, brand visibility, brand reputation, reputation management, media coverage, PR campaigns, press releases, crisis management, thought leadership, brand positioning, influencer relations, brand recognition, PR agency, PR solutions, PR strategies, media outreach, PR professionals, result-oriented PR, digital marketing, online marketing, internet marketing, digital strategy, SEO, search engine optimization, SEM, search engine marketing, PPC, pay-per-click, social media marketing, SMM, content marketing, email marketing, web analytics, data-driven marketing, AI-driven marketing, personalized marketing, online advertising, digital campaigns, brand visibility, brand recognition, digital presence, online success, ROI-driven marketing, 1-to-1 TV interview, TV interview services, TV interview agency, media interviews, interview preparation, media training, on-camera coaching, interview techniques, TV appearance, interview experts, interview coaching, media appearance, media communication, TV interview tips, interview strategies, TV interview preparation, interview skills, media spokesperson, interview coaching services, media appearance training, TV interview coaching"
+ogImage=""
+ogLocale="en_US"
+ogType="website"
+ogTitle="Contact Us | Top PR Agency Newsmaker Media and Communications"
+ogDescription="Get in touch to get your personalized PR strategies to excel your marketing planes and get better reach. Working with more than 300 media partners to provie you excellent brand experience."
+ogUrl="https://www.newsmakermediagroup.com/contact-us"
+ogSiteName="Newsmaker Media and Communications"
+author="Newsmaker Media and Communications"
+robots="index,follow"
+canonical="https://www.newsmakermediagroup.com/contact-us"
+distribution="global"
+revisitAfter="1 Day"
+googlebot="all"
+geo="india"
+ogCountryCode="india">
+
   <x-contact_form :contactUs="$contactUs"></x-contact_form>
   <x-partners :partners="$partners"></x-partners>
   <x-testimonials :testimonials="$testimonials"></x-testimonials>
